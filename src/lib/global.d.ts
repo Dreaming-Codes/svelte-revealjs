71d32dfe6a228cda0902f289dbe4426e0654df7f
DreamingCodes <me@dreaming.codes>
2024-02-08 20:00:16 +0100

    Rexported components

--- src/lib/global.d.ts
+++ src/lib/global.d.ts
@@ -1,4 +1,4 @@
-declare namespace svelteHTML {
+export declare namespace svelteHTML {
 	interface HTMLAttributes {
 		'on:in'?: (event: CustomEvent) => void
 		'on:out'?: (event: CustomEvent) => void
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,32 @@
-// Reexport your entry components here
+import Code from './components/code.svelte';
+import FitText from './components/fitText.svelte';
+import Markdown from './components/markdown.svelte';
+import Media from './components/media.svelte';
+import Notes from './components/notes.svelte';
+import Presentation from './components/presentation.svelte';
+import Slide from './components/slide.svelte';
+import Stack from './components/stack.svelte';
+import Step from './components/step.svelte';
+import Stretch from './components/stretch.svelte';
+import Vertical from './components/vertical.svelte';
+import type { PresentationOptions } from './types.d.ts';
+import type { svelteHTML } from './global.js';
+
+export {
+	Code,
+	FitText,
+	Markdown,
+	Media,
+	Notes,
+	Presentation,
+	Slide,
+	Stack,
+	Step,
+	Stretch,
+	Vertical
+};
+
+export type {
+	PresentationOptions,
+	svelteHTML
+};
